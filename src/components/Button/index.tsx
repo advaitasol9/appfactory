a82672c80ea2ab3ee3937dcec295ef93af5a4520
advaitasol9 <advaitasol9@gmail.com>
2023-04-16 12:53:27 +0530

    changes to button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import React, { FC, ReactNode } from 'react';
+import React, { forwardRef, ReactNode } from 'react';
 import Spinner from '../Spinner';
 import classes from './_style.module.scss';
 
@@ -13,38 +13,44 @@ export interface ButtonProps
   className?: string;
 }
 
-const Button: FC<ButtonProps> = ({
-  variant = 'Default',
-  isLoading = false,
-  leftElement,
-  rightElement,
-  children,
-  className,
-  ...props
-}) => {
-  return (
-    <button
-      className={classNames(classes['button'], className, {
-        [classes['button-default']]: variant == 'Default',
-        [classes['button-outline']]: variant == 'Outline',
-        [classes['button-link']]: variant == 'Link',
-        [classes['button-none']]: variant == 'None',
-      })}
-      {...props}
-    >
-      {isLoading ? (
-        <Spinner />
-      ) : (
-        <div className={classes['wrapper']}>
-          <>
-            {leftElement && leftElement}
-            {children}
-            {rightElement && rightElement}
-          </>
-        </div>
-      )}
-    </button>
-  );
-};
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      variant = 'Default',
+      isLoading = false,
+      leftElement,
+      rightElement,
+      children,
+      className,
+      ...props
+    },
+    ref
+  ) => {
+    return (
+      <button
+        ref={ref}
+        className={classNames(classes['button'], className, {
+          [classes['button-default']]: variant == 'Default',
+          [classes['button-outline']]: variant == 'Outline',
+          [classes['button-link']]: variant == 'Link',
+          [classes['button-none']]: variant == 'None',
+        })}
+        {...props}
+      >
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <div className={classes['wrapper']}>
+            <>
+              {leftElement && leftElement}
+              {children}
+              {rightElement && rightElement}
+            </>
+          </div>
+        )}
+      </button>
+    );
+  }
+);
 
 export default Button;
